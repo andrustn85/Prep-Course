284ed83dcc679b998f5fdb7ffbd7337d7027f26b
andrustn <rustandres@gmail.com>
2022-01-31 20:06:59 +0100

    tareaII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -108,9 +108,14 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-
-
+  if (Math.floor(Math.random() * 0)) {
+    return true
+  } else {
+    return false
+  }
 }
+
+
 
 function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3, devuelve "fizz"
@@ -154,7 +159,16 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  const getPrimes = numero => Array(numero - 1).fill().map((e, i) => 2 + i).filter((e, i, a) => a.slice(0, i).every(x => e % x !== 0));
+  if (numero < 2) return false;
+  if (numero === 2) return true;
+  for (var i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
+
+
 
 
   function esVerdadero(valor) {
@@ -171,7 +185,13 @@ function esPrimo(numero) {
   function tablaDelSeis() {
     //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
     //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-    //Escribe tu código aquí   
+    //Escribe tu código aquí  
+    let arrayTablaDel6 = []
+    for (let i = 0; i < 11; i++) {
+      arrayTablaDel6.push(6 * i)
+    }
+    return arrayTablaDel6
+
 
 
     function tieneTresDigitos(numero) {
@@ -188,6 +208,14 @@ function esPrimo(numero) {
       //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
       //Retornar el valor final.
       //Usar el bucle do ... while.
+      var a = numero;
+      var i = 0;
+      do {
+        i = i + 1;
+        a = a + 5;
+      }
+      while (i < 8);
+      return a;
     }
 
 
